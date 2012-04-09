86433debe5997e2189a8620cc42351cf18388828
John Gietzen <otac0n@gietzen.us>
2012-04-09 00:42:12 -0700

    Added reference implementation of `Descriptor`.

---
+++ reference/csharp/PortableVCSFormat.Tests/DescriptorTests.cs
@@ -0,0 +1,143 @@
+﻿namespace PortableVCSFormat.Tests
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DescriptorTests
+    {
+        [Test]
+        public void ReadFrom_FromANullStream_ThrowsException()
+        {
+            var stream = (Stream)null;
+
+            Assert.That(() => Descriptor.ReadFrom(stream), Throws.InstanceOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void ReadFrom_FromAnEmptyStream_ThrowsException()
+        {
+            using (var stream = new MemoryStream())
+            {
+                Assert.That(() => Descriptor.ReadFrom(stream), Throws.InstanceOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
+        public void ReadFrom_FromAStreamWithAValidFolderDescriptor_ReturnsTheDescriptor()
+        {
+            var descriptorBytes = Encoding.ASCII.GetBytes("28\0e82fe33199f25c242213ada825358e91c4261753\0t\03\0foo\0");
+            var expected = new Descriptor("e82fe33199f25c242213ada825358e91c4261753", DescriptorType.Folder, "foo");
+
+            using (var stream = new MemoryStream(descriptorBytes))
+            {
+                var descriptor = Descriptor.ReadFrom(stream);
+
+                Assert.That(descriptor, Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void ReadFrom_FromAStreamWithAValidFileDescriptor_ReturnsTheDescriptor()
+        {
+            var descriptorBytes = Encoding.ASCII.GetBytes("28\0e82fe33199f25c242213ada825358e91c4261753\0b\03\0foo\0");
+            var expected = new Descriptor("e82fe33199f25c242213ada825358e91c4261753", DescriptorType.File, "foo");
+
+            using (var stream = new MemoryStream(descriptorBytes))
+            {
+                var descriptor = Descriptor.ReadFrom(stream);
+
+                Assert.That(descriptor, Is.EqualTo(expected));
+            }
+        }
+
+        [Test]
+        public void ReadFrom_FromAStreamWithAnInvalidDescriptorType_ThrowsException()
+        {
+            var descriptorBytes = Encoding.ASCII.GetBytes("28\0e82fe33199f25c242213ada825358e91c4261753\0q\03\0foo\0");
+
+            using (var stream = new MemoryStream(descriptorBytes))
+            {
+                Assert.That(() => Descriptor.ReadFrom(stream), Throws.InstanceOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
+        public void ReadFrom_FromAStreamWithNonHexadecimalCharacters_ThrowsException()
+        {
+            var descriptorBytes = Encoding.ASCII.GetBytes("zz\0e82fe33199f25c242213ada825358e91c4261753\0b\03\0foo\0");
+            var expected = new Descriptor("e82fe33199f25c242213ada825358e91c4261753", DescriptorType.File, "foo");
+
+            using (var stream = new MemoryStream(descriptorBytes))
+            {
+                Assert.That(() => Descriptor.ReadFrom(stream), Throws.InstanceOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
+        public void ReadFrom_FromAStreamWithAZeroLengthName_ThrowsException()
+        {
+            var descriptorBytes = Encoding.ASCII.GetBytes("0\0\0t\03\0foo\0");
+
+            using (var stream = new MemoryStream(descriptorBytes))
+            {
+                Assert.That(() => Descriptor.ReadFrom(stream), Throws.InstanceOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
+        [TestCase("2")]
+        [TestCase("28")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c426175")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\0")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\03")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\03\0")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\03\0fo")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\03\0foo")]
+        public void ReadFrom_WithAnEOFAtVariousPlaces_ThrowsException(string value)
+        {
+            var descriptorBytes = Encoding.ASCII.GetBytes(value);
+
+            using (var stream = new MemoryStream(descriptorBytes))
+            {
+                Assert.That(() => Descriptor.ReadFrom(stream), Throws.InstanceOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
+        [TestCase("28\x01e82fe33199f25c242213ada825358e91c4261753\0b\03\0foo\0")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\x01b\03\0foo\0")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\x013\0foo\0")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\03\x01foo\0")]
+        [TestCase("28\0e82fe33199f25c242213ada825358e91c4261753\0b\03\0foo\x01")]
+        public void ReadFrom_WithANonNullByteWhereNullBytesAreExpected_ThrowsException(string value)
+        {
+            var descriptorBytes = Encoding.ASCII.GetBytes(value);
+
+            using (var stream = new MemoryStream(descriptorBytes))
+            {
+                Assert.That(() => Descriptor.ReadFrom(stream), Throws.InstanceOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
+        public void ReadFrom_StreamThatHasBeenWrittenTo_RoundTripsTheValue()
+        {
+            var expected = new Descriptor("e82fe33199f25c242213ada825358e91c4261753", DescriptorType.File, "foo");
+
+            using (var stream = new MemoryStream())
+            {
+                expected.WriteTo(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var actual = Descriptor.ReadFrom(stream);
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+        }
+    }
+}
---
+++ reference/csharp/PortableVCSFormat/Descriptor.cs
@@ -0,0 +1,243 @@
+﻿namespace PortableVCSFormat
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    /// <summary>
+    /// Specifies whether a <see cref="Descriptor"/> represents a file or a folder.
+    /// </summary>
+    public enum DescriptorType
+    {
+        /// <summary>
+        /// Indicates that a <see cref="Descriptor"/> represents a file.
+        /// </summary>
+        File,
+
+        /// <summary>
+        /// Indicates that a <see cref="Descriptor"/> represents a folder.
+        /// </summary>
+        Folder,
+    }
+
+    /// <summary>
+    /// Describes the filename and contents of a file or folder.
+    /// </summary>
+    public class Descriptor
+    {
+        private readonly string objectName;
+        private readonly DescriptorType descriptorType;
+        private readonly string name;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Descriptor"/> class.
+        /// </summary>
+        /// <param name="objectName">The object name of the contents of the new descriptor.</param>
+        /// <param name="descriptorType">The type of the new descriptor.</param>
+        /// <param name="name">The name of the new descriptor.</param>
+        public Descriptor(string objectName, DescriptorType descriptorType, string name)
+        {
+            if (string.IsNullOrEmpty(objectName))
+            {
+                throw new ArgumentNullException("objectName");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            this.objectName = objectName;
+            this.descriptorType = descriptorType;
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Gets the object name of the contents of this descriptor.
+        /// </summary>
+        public string ObjectName
+        {
+            get { return this.objectName; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating the type of this descriptor.
+        /// </summary>
+        public DescriptorType DescriptorType
+        {
+            get { return this.descriptorType; }
+        }
+
+        /// <summary>
+        /// Gets the name of this descriptor.
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public static bool operator ==(Descriptor left, Descriptor right)
+        {
+            return object.Equals(left, right);
+        }
+
+        public static bool operator !=(Descriptor left, Descriptor right)
+        {
+            return !object.Equals(left, right);
+        }
+
+        /// <summary>
+        /// Reads a descriptor from a stream.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> from which to read the descriptor.</param>
+        /// <returns>The new descriptor.</returns>
+        public static Descriptor ReadFrom(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var objectNameLength = ReadLength(stream);
+            var objectName = ReadText(stream, objectNameLength);
+            var descriptorType = ReadType(stream);
+            var nameLength = ReadLength(stream);
+            var name = ReadText(stream, nameLength);
+
+            return new Descriptor(objectName, descriptorType, name);
+        }
+
+        /// <summary>
+        /// Writes a descriptor to a stream.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> to which to write the descriptor.</param>
+        public void WriteTo(Stream stream)
+        {
+            byte[] buffer;
+
+            buffer = Encoding.ASCII.GetBytes(Convert.ToString(Encoding.UTF8.GetByteCount(this.objectName), 16));
+            stream.Write(buffer, 0, buffer.Length);
+            stream.WriteByte(0);
+
+            buffer = Encoding.UTF8.GetBytes(this.objectName);
+            stream.Write(buffer, 0, buffer.Length);
+            stream.WriteByte(0);
+
+            stream.WriteByte((byte)(this.DescriptorType == DescriptorType.File ? 'b' : 't'));
+            stream.WriteByte(0);
+
+            buffer = Encoding.ASCII.GetBytes(Convert.ToString(Encoding.UTF8.GetByteCount(this.name), 16));
+            stream.Write(buffer, 0, buffer.Length);
+            stream.WriteByte(0);
+
+            buffer = Encoding.UTF8.GetBytes(this.name);
+            stream.Write(buffer, 0, buffer.Length);
+            stream.WriteByte(0);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Descriptor;
+            if (object.ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            return
+                this.objectName == other.objectName &&
+                this.descriptorType == other.descriptorType &&
+                this.name == other.name;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 0x51ED270B;
+            hash = (hash * -0x25555529) + this.objectName.GetHashCode();
+            hash = (hash * -0x25555529) + this.descriptorType.GetHashCode();
+            hash = (hash * -0x25555529) + this.name.GetHashCode();
+
+            return hash;
+        }
+
+        private static int ReadLength(Stream stream)
+        {
+            var result = new StringBuilder();
+            int c;
+            while ((c = stream.ReadByte()) > 0)
+            {
+                if ((c >= '0' && c <= '9') ||
+                    (c >= 'A' && c <= 'F') ||
+                    (c >= 'a' && c <= 'f'))
+                {
+                    result.Append((char)c);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Read non-hexadecimal digit.");
+                }
+            }
+
+            if (c == -1)
+            {
+                throw new InvalidOperationException("EOF reached while reading descriptor.");
+            }
+
+            var length = Convert.ToInt32(result.ToString(), 16);
+            if (length == 0)
+            {
+                throw new InvalidOperationException("Read zero length for name.");
+            }
+
+            return length;
+        }
+
+        private static string ReadText(Stream stream, int length)
+        {
+            var buffer = new byte[length];
+
+            int read = stream.Read(buffer, 0, length);
+            if (read != length)
+            {
+                throw new InvalidOperationException("EOF reached while reading descriptor.");
+            }
+
+            AssertNullByte(stream);
+
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        private static DescriptorType ReadType(Stream stream)
+        {
+            var type = stream.ReadByte();
+            if (type == -1)
+            {
+                throw new InvalidOperationException("EOF reached while reading descriptor.");
+            }
+
+            AssertNullByte(stream);
+
+            switch (type)
+            {
+                case (int)'T':
+                case (int)'t': return DescriptorType.Folder;
+                case (int)'B':
+                case (int)'b': return DescriptorType.File;
+                default:
+                    throw new InvalidOperationException("Unknown descriptor type read.");
+            }
+        }
+
+        private static void AssertNullByte(Stream stream)
+        {
+            var next = stream.ReadByte();
+            if (next == -1)
+            {
+                throw new InvalidOperationException("EOF reached while reading descriptor.");
+            }
+            else if (next != 0)
+            {
+                throw new InvalidOperationException("Expected null byte, got non-zero byte.");
+            }
+        }
+    }
+}
